decca69163f49433dd53d5f155295e7fec036cfe
MaciejMatczak2001 <maciej.matczak2001@gmail.com>
2024-03-27 21:54:08 +0100

    Adding icons and static folder

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "recipes.apps.RecipiesConfig"
+    "recipes.apps.RecipiesConfig",
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
