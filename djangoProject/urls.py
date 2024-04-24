6cbe8b8f8e5de7c9130c24f1f13cd9283f2bd7fe
MaciejMatczak2001 <maciej.matczak2001@gmail.com>
2024-04-24 16:06:06 +0200

    User authentication added

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -6,13 +6,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path("", views.home, name="home")
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path("", Home.as_view(), name="home")
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("recipes.urls")),
-    path("recipes/about", include("recipes.urls"))
+    path("recipes/about", include("recipes.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
