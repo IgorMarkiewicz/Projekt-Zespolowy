6765f35e4889744dc9c54d20121331d09d54518e
IgorMarkiewicz <markiewicz.igor.ezn@gmail.com>
2024-04-25 01:48:41 +0200

    broken pipe error check

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import allergen_list
+from .views import user_list
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("recipes/about", views.about, name="about"),
+    path('allergens/', allergen_list, name='allergen_list'),
+    path('users/', user_list, name='user_list'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from recipes.models import Allergen
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def allergen_list(request):
+    allergens = Allergen.objects.all()
+    return render(request, 'allergen_list.html', {'allergens': allergens})
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'user_list.html', {'users': users})
 
 
 def about(request):
