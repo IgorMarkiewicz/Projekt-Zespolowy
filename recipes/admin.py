fbf44da8c29e0a5166bfc7971798158391c11b36
MaciejMatczak2001 <maciej.matczak2001@gmail.com>
2024-04-24 16:15:22 +0200

    Admin panel configuration

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from recipes.models import Recipe, Ingredient, Category, Allergen, Rating
+
 # Register your models here.
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Category)
+admin.site.register(Allergen)
+admin.site.register(Rating)
+
