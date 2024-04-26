c9833f2c1246ae4f444b6ad57afbfa5b153eb284
Jakub Blacha <Jakub.blacha44@gmail.com>
2024-04-27 01:05:48 +0200

    Recipes detail and Account recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from recipes.models import Recipe
@@ -13,6 +14,10 @@ def recipe_grid_alt_view(request):
 def about(request):
     return render(request, 'about.html')
 
-def recipe_detail(request, recipe_id):
+def recipe_detail(request, recipe_id,):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     return render(request, 'recipe_detail.html', {'recipe': recipe})
+def account_recipes(request, user_id,):
+    user = get_object_or_404(User, pk=user_id)
+    recipes_id = Recipe.objects.filter(user=user)
+    return render(request, 'account_recipes.html', {'recipes': recipes_id, 'user': user})
